53fd42e994dd784c3825ab41c629a0233b5e66a0
Victor <VictorCarmona9035@hotmail.com>
2023-03-21 19:15:38 +0100

    3-Creacion de tiendas en nuestra REST API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,7 +7,7 @@ print("Nombre aplicacion: ", __name__)
 #Declaracion de lista para almacenar los datos
 storesList = [
     {
-        "name":"Mi tienda",
+        "name":"Tienda 1",
         "items":[
             {
                 "name": "Silla",
@@ -17,13 +17,24 @@ storesList = [
     }
 ]
 
-"""sumary_line
-Devolvera la lista de stores --->storesList
-Keyword arguments:
-argument -- description
-Return: return_description
-"""
 
-@app.get("/store")
+#Obtenemos todas las tiendas de la lista
+@app.get("/store")     #http://127.0.0.1:5000/store
 def get_stores():
     return {"stores": storesList}
+
+
+#Creamos una tienda
+@app.post("/store")     #http://127.0.0.1:5000/store
+def create_store():
+    try:
+        #Obtenemos el dato que nos ha enviado el cliente
+        request_data = request.get_json()
+        #Montamos la nueva tienda
+        new_store = {'name':request_data["name"], "items":[]}
+        #Agregamos el nuevo elemento a la lista
+        storesList.append(new_store)
+        #Retornamos el nuevo elemento creado
+        return new_store, 201
+    except Exception as ex:
+        print(f'Ocurrio un error {ex},{TypeError}')
