9d4aef483c5dfcd9b1c6f06b8eee3b98e3c815d4
Victor <VictorCarmona9035@hotmail.com>
2023-03-21 18:50:34 +0100

    2-Nuestro primer REST API endpoint

--- app.py
+++ app.py
@@ -3,3 +3,27 @@ from flask import Flask
 app = Flask(__name__)
 
 print("Nombre aplicacion: ", __name__)
+
+#Declaracion de lista para almacenar los datos
+storesList = [
+    {
+        "name":"Mi tienda",
+        "items":[
+            {
+                "name": "Silla",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+"""sumary_line
+Devolvera la lista de stores --->storesList
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+@app.get("/store")
+def get_stores():
+    return {"stores": storesList}
